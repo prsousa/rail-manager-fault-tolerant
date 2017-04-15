92030e74e5a0fc9735e8cc75c236371f33e7bf9a
Basofe <helder_m_p_novais@hotmail.com>
2017-04-15 17:11:20 +0100

    Execution time added

--- src/main/java/pt/uminho/sdc/railmanager/RailManagerTester.java
+++ src/main/java/pt/uminho/sdc/railmanager/RailManagerTester.java
@@ -13,7 +13,7 @@ public class RailManagerTester {
 
     private final RailManagerSupplier supplier;
 
-    @FunctionalInterface
+    //@FunctionalInterface
     public interface RailManagerSupplier {
 
         RailManager get() throws Exception;
@@ -32,6 +32,9 @@ public class RailManagerTester {
         RailManager r = supplier.get();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long start;
+        long stopTime;
+        long execution;
 
         String line;
         while (true) {
@@ -43,24 +46,54 @@ public class RailManagerTester {
             try {
                 switch (method) {
                     case "access":
+                        start = System.currentTimeMillis();
                         boolean access = r.access(tokens[1], Integer.valueOf(tokens[2]), tokens[3].charAt(0));
+                        stopTime = System.currentTimeMillis();
+                        execution =  stopTime - start;
+                        System.out.println("\n---------------------------------------------------------");
                         System.out.println(line + ": " + access);
+                        System.out.println("'access' execution time  -> " + execution + " miliseconds");   
+                        System.out.println("---------------------------------------------------------");
                         break;
                     case "enter":
+                        start = System.currentTimeMillis();
                         r.enter(tokens[1], Integer.valueOf(tokens[2]), tokens[3].charAt(0));
+                        stopTime = System.currentTimeMillis();
+                        execution =  stopTime - start;
+                        System.out.println("\n--------------------------------------------------------");
                         System.out.println(line);
+                        System.out.println("'enter' execution time  -> " + execution + " miliseconds");
+                        System.out.println("--------------------------------------------------------");
                         break;
                     case "leave":
+                        start = System.currentTimeMillis();
                         r.leave(tokens[1], Integer.valueOf(tokens[2]), tokens[3].charAt(0));
+                        stopTime = System.currentTimeMillis();
+                        execution =  stopTime - start;
+                        System.out.println("\n--------------------------------------------------------");
                         System.out.println(line);
+                        System.out.println("'leave' execution time  -> " + execution + " miliseconds");
+                        System.out.println("--------------------------------------------------------");
                         break;
                     case "getPositions":
+                        start = System.currentTimeMillis();
                         Map<Character, Integer> positions = r.getPositions(tokens[1]);
+                        stopTime = System.currentTimeMillis();
+                        execution =  stopTime - start;
+                        System.out.println("\n---------------------------------------------------------------");
                         System.out.println(line + ": " + positions);
+                        System.out.println("'getPositions' execution time  -> " + execution + " miliseconds"); 
+                        System.out.println("---------------------------------------------------------------");
                         break;
                     case "getAlarms":
+                        start = System.currentTimeMillis();
                         List<String> alarms = r.getAlarms();
+                        stopTime = System.currentTimeMillis();
+                        execution =  stopTime - start;
+                        System.out.println("\n------------------------------------------------------------");
                         System.out.println(line + ": " + alarms);
+                        System.out.println("'getAlarms' execution time  -> " + execution + " miliseconds"); 
+                        System.out.println("------------------------------------------------------------");
                 }
             } catch (Exception e) {
                 logger.trace("client error {}", e);
